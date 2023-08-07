0d6e4530ee0084eb62673f902736d9afa00875e3
김우빈 <kwb020312@naver.com>
2023-08-07 23:29:35 +0900

    Feat: Socket 연결

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express')
 const app = express()
+
+const http = require('http')
+const server = http.createServer(app)
+const { Server } = require('socket.io')
+const io = new Server(server, { pingInterval: 2000, pingTimeout: 5000 })
+
 const port = 3000
 
 app.use(express.static('public'))
@@ -8,6 +14,25 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+const players = {}
+
+io.on('connection', (socket) => {
+  console.log('유저가 연결되었습니다.')
+  players[socket.id] = {
+    x: 500 * Math.random(),
+    y: 500 * Math.random()
+  }
+
+  io.emit('updatePlayers', players)
+
+  socket.on('disconnect', (reason) => {
+    delete players[socket.id]
+    io.emit('updatePlayers', players)
+  })
+
+  console.log(players)
+})
+
+server.listen(port, () => {
+  console.log(`서버가 ${port}포트로 시작되었습니다.`)
 })
--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,8 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
+const socket = io()
+
 const scoreEl = document.querySelector('#scoreEl')
 
 canvas.width = innerWidth
@@ -10,6 +12,23 @@ const x = canvas.width / 2
 const y = canvas.height / 2
 
 const player = new Player(x, y, 10, 'white')
+const players = {}
+
+socket.on('updatePlayers', (backendPlayers) => {
+  for (const id in backendPlayers) {
+    const backendPlayer = backendPlayers[id]
+
+    if (!players[id]) {
+      players[id] = new Player(backendPlayer.x, backendPlayer.y, 10, 'white')
+    }
+  }
+
+  for (const id in players) {
+    if (!backendPlayers[id]) {
+      delete players[id]
+    }
+  }
+})
 
 let animationId
 let score = 0
@@ -18,7 +37,10 @@ function animate() {
   c.fillStyle = 'rgba(0, 0, 0, 0.1)'
   c.fillRect(0, 0, canvas.width, canvas.height)
 
-  player.draw()
+  for (const id in players) {
+    const player = players[id]
+    player.draw()
+  }
 }
 
 animate()
